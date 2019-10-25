7aeec225848e30d7404aa49fb90c7ac87450267c
ma-daria <Can't finish GitHub sharing process 				Successfully created project 'test_ITOB' on GitHub, but initial commit failed: 				*** Please tell me who you are. 				 				Run 				 				git config --global user.email "you@example.com" 				git config --global user.name "Your Name" 				 				to set your account's default identity. 				Omit --global to set the identity only in this repository. 				 				unable to auto-detect email address (got 'ma-daria@madaria-UX32LA.(none)')>
2019-10-25 21:51:07 +0300

    add bd

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +37,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  return next();
 });
 
 module.exports = app;
